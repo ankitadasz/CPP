8520502981129e8abad692020906c8d1e10b88a0
Ankita Das <daszankita23@gmail.com>
2025-04-29 19:35:24 +0530

    pointerToobject

---
+++ OOPS/PointerToObject.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+public:
+    int length;
+    int breadth;
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+/// THE OBJECT IS CREATED INSIDE STACK///
+// int main()
+// {
+//     Rectangle r;
+//     Rectangle *p;
+//     p = &r;
+//     p->length = 10;
+//     p->breadth = 20;
+//     cout << p->area();
+// }
+/// THE OBJECT IS CREATED INSIDE HEAP///
+int main()
+{
+    Rectangle *p;
+    p = new Rectangle();
+    Rectangle *q = new Rectangle();
+    p->length = 10;
+    p->breadth = 20;
+    cout << p->area();
+}
---
+++ OOPS/PointerToObject.md
@@ -0,0 +1,11 @@
+In java you cant create object in stack but in C++ you can create object in java as well as c++
+
+method to create object:-
+in stack
+Rectangle r;
+in heap
+Rectangle *p=new Rectangle();
+
+dot operator is used to access the member of the object using variable name whereas the -> operator is used to access the member of the object using pointer on an object
+
+pointer takes 2 byte of memory but it depends upon the compiler so it can also take 4 byte
