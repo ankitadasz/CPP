49e33a17bb7c19c7a8c0c4a8d1db25d64f4e51fa
Ankita Das <daszankita23@gmail.com>
2025-05-04 22:03:46 +0530

    ScopeResolutionOperator

--- OOPS/ScopeResolutionOperator.cpp
+++ OOPS/ScopeResolutionOperator.cpp
@@ -1,3 +1,4 @@
+// scope resolution says that the scope of this function belongs to the class rectangle
 #include <iostream>
 using namespace std;
 class Rectangle
@@ -7,13 +8,64 @@ private:
     int breadth;
 
 public:
-    int area()
-    {
-        return length * breadth;
-    }
+    Rectangle();
+    Rectangle(int l, int b);
+    Rectangle(Rectangle &r);
+    int getLength() { return length; };
+    int getBreadth() { return breadth; };
+    void setLength(int l);
+    void setBreadth(int b);
+    bool isSquare();
+    ~Rectangle();
+    int area();
     int perimeter();
 };
+Rectangle::Rectangle()
+{
+    length = 1;
+    breadth = 1;
+}
+Rectangle::Rectangle(int l, int b)
+{
+    length = l;
+    breadth = b;
+}
+Rectangle::Rectangle(Rectangle &r)
+{
+    length = r.length;
+    breadth = r.breadth;
+}
+void Rectangle::setLength(int l)
+{
+    length = l;
+}
+void Rectangle::setBreadth(int b)
+{
+    breadth = b;
+}
+
 int Rectangle::perimeter()
-{ // scope resolution says that the scope of this function belongs to the class rectangle
+{
     return 2 * (length + breadth);
+}
+int Rectangle::area()
+{
+    return length * breadth;
+}
+bool Rectangle ::isSquare()
+{
+    return length == breadth;
+}
+Rectangle::~Rectangle()
+{
+    cout << "Rectangle Destroyed";
+}
+int main()
+{
+    Rectangle r(10,10);
+    cout <<"Area"<< r.area()<<endl;
+    cout << r.perimeter()<<endl;
+    if(r.isSquare()){
+        cout<<"yes"<<endl;
+    }
 }
