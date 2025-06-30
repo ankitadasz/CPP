4ab2c12274f1834dc6ee65da44c422a365b79c23
Ankita Das <daszankita23@gmail.com>
2025-06-30 16:04:32 +0530

    AbstractClass

--- Polymorphism/AbstractClasses.cpp
+++ Polymorphism/AbstractClasses.cpp
@@ -1,1 +1,26 @@
-  
+#include<iostream>
+using namespace std;
+class Base{
+    public:
+    virtual void Fun1()=0;
+    virtual void Fun2()=0;
+};
+
+class Derived: public Base{
+    public:
+    void Fun1(){
+        cout<<"Fun1 of derived class"<<endl;
+    }
+    void Fun2(){
+        cout<<"Fun2 of derived class"<<endl;
+    }
+};
+int main(){
+    Derived d;
+    d.Fun1();
+    d.Fun2();
+    Base *p=new Derived();
+    p->Fun1();
+    p->Fun2();
+
+}
