1927f0717aa8607317b328409e47ac3e25b5b5b4
Ankita Das <daszankita23@gmail.com>
2026-01-27 08:28:00 +0530

    Sum of array elements

--- Arrays/sumofarrayelements.cpp
+++ Arrays/sumofarrayelements.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=5,i,sum=0;
+    int n,i,sum=0;
+    cout<<"Enter the no of elemnts you want to enter:";
+    cin>>n;
     int a[n];
-    cout<<"Give the elements of array:";
-    cout<<endl;
+    cout<<"Enter the elemnts you want to enter:"<<endl;
     for(i=0;i<n;i++){
         cin>>a[i];
     }
-    cout<<"Array elements are:";
-    cout<<endl;
+    cout<<"Elemnts of the array are:";
     for(i=0;i<n;i++){
-        cout<<a[i]<<endl;
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"The sum of the elemnts of the array is:";
+      for(i=0;i<n;i++){
         sum=sum+a[i];
     }
-    cout<<"Sum is:"<<sum;
+    cout<<sum;
 }
