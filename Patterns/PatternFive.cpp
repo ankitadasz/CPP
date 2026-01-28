bdcf077970f00b58102485610e328e3476abbe8f
Ankita Das <daszankita23@gmail.com>
2026-01-28 21:57:43 +0530

    flipped inverted rightangle triangle

---
+++ Patterns/PatternFive.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,k,n=4;
+    for(i=0;i<n;i++){
+        for(j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(k=0;k<n-i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
