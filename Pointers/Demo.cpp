d9b35ebbf412db653e6f36529de1429cfcbef3e7
Ankita Das <daszankita23@gmail.com>
2026-01-28 22:59:55 +0530

    Syntax for pointer

--- Pointers/Demo.cpp
+++ Pointers/Demo.cpp
@@ -1,17 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int A[5]={1,2,3,4,5};
-    int *p=A,*q=&A[4];
-    cout<<q-p;
-    // cout<<*p<<endl;
-    // cout<<*(p+3)<<endl;
-    // // p++;
-    // cout<<*p;
-    // cout<<p;
-    // for(int i=0;i<5;i++){
-    //     cout<<*p<<endl;
-    //     p++;
-    // }
-    // cout<<p;
+    int a=10;
+    int *p=&a;
+
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p<<endl;
+
 }
