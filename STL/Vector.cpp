a6d297dc22c7482e8a42b39c70aa09f76c30c4f6
Ankita Das <daszankita23@gmail.com>
2026-01-31 22:16:10 +0530

    STL

---
+++ STL/Vector.cpp
@@ -0,0 +1,3 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
---
+++ STL/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+int main(){
+    pair<int,int> arr[]{{1,2},{2,3}};
+    cout<<arr[1].first<<" "<<arr[0].second;
+}
